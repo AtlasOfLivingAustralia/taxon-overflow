6d34d263282174985ab9b0814bd4270ded15eb13
Angel Ruiz <aruizca@gmail.com>
2015-03-20 14:30:38 +1100

    WIP task #22 -> Create automatic tests for /ws/question/search endpoint

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,31 +5,14 @@ class BootStrap {
     def elasticSearchService
     def customJSONMarshallers
     def grailsApplication
+    def sourceService
 
     def init = { servletContext ->
         // Reference the service to make sure it's loaded and initialised
         elasticSearchService.ping()
         customJSONMarshallers.register()
 
-        //add biocache instance
-        def biocacheSource = Source.findByName("biocache")
-        if(!biocacheSource){
-            biocacheSource = new Source(name:"biocache", wsBaseUrl: grailsApplication.config.biocacheService.recordUrl, uiBaseUrl: grailsApplication.config.biocacheWebapp.recordUrl)
-        } else {
-            biocacheSource.wsBaseUrl = grailsApplication.config.biocacheService.recordUrl
-            biocacheSource.uiBaseUrl = grailsApplication.config.biocacheWebapp.recordUrl
-        }
-        biocacheSource.save(flush:true)
-
-        //add ecodata instance
-        def ecodataSource = Source.findByName("ecodata")
-        if(!ecodataSource){
-            ecodataSource = new Source(name:"ecodata", uiBaseUrl: grailsApplication.config.pigeonhole.recordUrl, wsBaseUrl: grailsApplication.config.ecodata.recordUrl)
-        } else {
-            ecodataSource.wsBaseUrl = grailsApplication.config.ecodata.recordUrl
-            ecodataSource.uiBaseUrl = grailsApplication.config.pigeonhole.recordUrl
-        }
-        ecodataSource.save(flush:true)
+        sourceService.init()
     }
 
     def destroy = {
