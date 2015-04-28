010ea9ee06a22067778b76d05036a40dc2c866ef
Angel Ruiz <aruizca@gmail.com>
2015-04-28 16:44:14 +1000

    Fixes bug #39 -> The "Your activity summary" page is currently throwing error

--- grails-app/controllers/au/org/ala/taxonoverflow/UserController.groovy
+++ grails-app/controllers/au/org/ala/taxonoverflow/UserController.groovy
@@ -25,8 +25,8 @@ class UserController {
             def rec =  elasticSearchService.getOccurrenceData(q.occurrenceId)
             log.debug "rec = ${rec}"
             // http://images.ala.org.au/store/2/5/8/e/e90caa4c-a0b7-4552-b4fb-6df415f6e852/thumbnail_square
-            q.metaClass.imageUrls = rec.imageUrls
-            q.metaClass.imageIds = rec.imageIds
+            q.metaClass.imageUrls = rec?.imageUrls
+            q.metaClass.imageIds = rec?.imageIds
         }
     }
 
